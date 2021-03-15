e4baf8512ec0d7207d607d37d5a64898dd163cac
Yonatan Reicher <yony252525@gmail.com>
2021-03-15 15:01:43 +0200

    Switched type back to use

--- src/input.rs
+++ src/input.rs
@@ -1,5 +1,5 @@
-pub type Key = tcod::input::Key;
-pub type KeyCode = tcod::input::KeyCode;
+pub use tcod::input::Key;
+pub use tcod::input::KeyCode;
 
 pub trait InputHandler {
     fn wait_for_keypress(&mut self) -> Key;
