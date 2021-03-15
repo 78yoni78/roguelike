4f336d30c9e21afd6762441d4558f4b4673a9839
Yonatan Reicher <yony252525@gmail.com>
2021-03-15 15:04:21 +0200

    Switched constant names to UPPERCASE

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ use window::Window;
 use game::Game;
 
 fn main() {
-    const fps_limit: i32 = 20;
-    const size: (u16, u16) = (80, 50);
-    const title: &str = "My Game";
+    const FPS_LIMIT: i32 = 20;
+    const SIZE: (u16, u16) = (80, 50);
+    const TITLE: &str = "My Game";
 
     //  Create a 
-    let mut window = Window::new(window::Config { title: title.to_string(), size, fps_limit });
+    let mut window = Window::new(window::Config {
+        title: TITLE.to_string(),
+        size: SIZE,
+        fps_limit: FPS_LIMIT
+    });
     let mut game = Game::new();
 
     while !window.closed() {
